599f557c413fdf568f8a0ea4ed137d46f2af183d
Maxime <maxime.larchez@orange.fr>
2023-03-07 17:57:55 +0100

    modif_repo

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -11,16 +11,23 @@ import { Slide } from 'react-slideshow-image'
 
 const SlideShow = () => {
     const pictures = data[1].pictures; // Mettre dans le tableau l'id ? comment faire ? 
-
+    const Propriete ={
+        duration: 2000,
+        transitionDuration: 5000,
+        autoPlay:true,
+        showIndicators: false,
+        
+    }
 
 
     return (
-        <section>
+        <section className='section_Caroussel'>
             <div className='Caroussel_Container'>
-                <Carousel className='Caroussel'>
+                <Carousel  {...Propriete} className='Caroussel'>
                     {pictures.map((picture, index) => (
                         <img className='img_Caroussel' key={index} src={picture} alt={`Picture ${index + 1}`} />
                     ))}
+                    
                 </Carousel>
 
             </div>
